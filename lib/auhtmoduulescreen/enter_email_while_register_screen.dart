1ce0fd2d659fc683f52bea010b3f3cbac66d75f9
maazsaleem1 <124529452+maazsaleem1@users.noreply.github.com>
2025-03-12 19:50:38 +0500

    changes done

--- lib/auhtmoduulescreen/enter_email_while_register_screen.dart
+++ lib/auhtmoduulescreen/enter_email_while_register_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:thai_friendly_app/res/appcolors.dart';
+import 'package:thai_friendly_app/onboardingfolder/custom_button.dart';
+import 'package:thai_friendly_app/res/fonts.dart';
+
+import '../res/appcolors.dart';
 
 class RegisterEmail extends StatelessWidget {
   const RegisterEmail({super.key});
@@ -10,11 +13,34 @@ class RegisterEmail extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.pinksahdebackground,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            height: 350.h,
+            height: 150.h,
             width: 1.sw,
-          )
+            color: const Color(0xFF00796B).withOpacity(0.5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'We will send a code to the email below',
+                  style: interFont(
+                    fontsize: 15,
+                    fontweight: FontWeight.w700,
+                    color: AppColors.white,
+                  ),
+                ),
+                30.verticalSpace,
+                AppButton(
+                  horizontalMargin: 70.w,
+                  text: "EMAIL CODE",
+                  backgroundColor: const Color(0xFF00796B),
+                  fontweight: FontWeight.w500,
+                  onPress: () {},
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
